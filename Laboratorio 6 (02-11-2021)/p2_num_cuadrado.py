253a1bc7b157056ca35774e8ca805c3340e71c19
Italo <itamikurumi@gmail.com>
2021-11-05 14:57:09 -0500

    Problema 2: Implementación de búsqueda binaria para sacar el cuadrado de un número

---
+++ Laboratorio 6 (02-11-2021)/p2_num_cuadrado.py
@@ -0,0 +1,17 @@
+# Problema 2
+def sqrtBinary(number):
+    # Determinar si un número es cuadrado usando la búsqueda binaria
+    N = number
+    L = 0
+    R = N - 1
+    while L <= R:
+        mid = L + (R - L) / 2
+        mid = int(mid)
+        if mid * mid == number:
+            return mid
+        if mid * mid < number:
+            L = mid + 1
+        else:
+            R = mid - 1
+    return -1
+
