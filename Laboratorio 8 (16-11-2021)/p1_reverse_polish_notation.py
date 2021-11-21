bf88fe06c1bc7bbb6728a8e187e49d7be79212a8
Italo <iccoscco@unsa.edu.pe>
2021-11-21 06:23:51 -0500

    Lab 8: uso de método split para dividir la cadena en un array

--- Laboratorio 8 (16-11-2021)/p1_reverse_polish_notation.py
+++ Laboratorio 8 (16-11-2021)/p1_reverse_polish_notation.py
@@ -2,14 +2,27 @@
 def reverse_polish_notation(cadena):
     # Evaluar la reversión polaca inversa
     pila = []
-    for i in range(0,len(cadena),2):
-        if cadena[i] != "+" and cadena[i] != "-" and cadena[i] != "*" and cadena[i] != "/":
-            pila.append(int(cadena[i]))
+    arrayCad = cadena.split(" ")
 
+    for i in range(arrayCad):
+        if arrayCad[i] != "+" and arrayCad[i] != "-" and arrayCad[i] != "*" and arrayCad[i] != "/":
+            
+        # else:
+        #     n1 = pila.pop()
+        #     n2 = pila.pop()
+        #     value = 0;
+        #     # Revisamos que operación hacer
+        #     if auxCad[i] == "+":
+        #         value = n1 + n2
+        #     if auxCad[i] == "-":
+        #         value = n1 - n2
+        #     if auxCad[i] == "*":
+        #         value = n1 * n2
+        #     if auxCad[i] == "/":
+        #         value = n1 / n2
+        #     pila.append(value)
     print(pila)
 
-
-
-
-
-reverse_polish_notation("3 + 5")   
+reverse_polish_notation("2 1 5 3 *")   
+# reverse_polish_notation("2 1 + 3 *")   
+# reverse_polish_notation("4 13 5 / +")   
