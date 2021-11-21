ff76f8b4187bd5ea0267d6913686494db5dadfe2
Italo <iccoscco@unsa.edu.pe>
2021-11-21 08:04:21 -0500

    Lab 8: problema 2 implementación de solución

---
+++ Laboratorio 8 (16-11-2021)/p2_minimun_add_to_make_parentheses_valid.py
@@ -0,0 +1,19 @@
+# Problema 2:
+
+def minimun_size_valid(cadena):
+    # Determinar cuantos paréntesis válidos se ingresan al programa
+    pila = []
+    for i in range(len(cadena)):
+        # Revisar su la pila contiene elementos o no
+        if len(pila) > 0:
+            print(f"{pila} =======")
+            if pila[len(pila)-1] != cadena[i] and cadena[i] == ")":
+                pila.pop()
+            else:
+                pila.append(cadena[i])
+
+        else: 
+            pila.append(cadena[i])
+    print(len(pila))
+
+
